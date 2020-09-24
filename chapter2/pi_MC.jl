194fad5e0b21cd9007287b8d207002189c34fe95
Keisuke Otaki <keisuke.otaki.jp@gmail.com>
2020-09-24 20:00:30 +0900

    update code

--- chapter2/pi_MC.jl
+++ chapter2/pi_MC.jl
@@ -6,10 +6,7 @@ function compute(;n_iter=1_000_000, print_step=1_000)
     for i in 1:n_iter
         xy = rand(2)
         (norm(xy) < 1) && (n_in += 1)
-
-        if i % print_step == 0
-            println("$i $(n_in / i)")
-        end
+        (i % print_step == 0) && println("$i $(n_in / i)")
     end
 
     println("true: $(pi/4)")
